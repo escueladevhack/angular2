bc77e1fe797e99a5fe0c496bfed1bc0bb479e3c6
Diego Coy <diegoct11@hotmail.com>
2016-11-24 16:07:35 -0500

    tarea: convertir promesa a observable

--- ng2/aplicacionDePrueba/src/app/main/main.component.ts
+++ ng2/aplicacionDePrueba/src/app/main/main.component.ts
@@ -18,15 +18,28 @@ export class MainComponent implements OnInit {
   }
 
   getEmotions() {
-    this.emotionsService.getEmotions(this.laURL).then(data => {
-      this.resultados = Object.keys(data[0].scores).map(resultado => {
-        return {
-          emocion: resultado,
-          puntaje: data[0].scores[resultado]
-        }
-      })
-
-    });
+    this.emotionsService.getEmotions(this.laURL).subscribe(
+      response =>  {
+        const primerElemento = response.json()[0];
+        this.resultados = Object.keys(primerElemento.scores).map(resultado => {
+          return {
+            emocion: resultado,
+            puntaje: primerElemento.scores[resultado]
+          }
+        })
+      },
+      err => {
+        console.log(err)
+      },
+      () => {
+        console.log('end')
+      }
+    )
   }
-
+// this.resultados = Object.keys(data[0].scores).map(resultado => {
+//         return {
+//           emocion: resultado,
+//           puntaje: data[0].scores[resultado]
+//         }
+//       })
 }
--- ng2/aplicacionDePrueba/src/app/shared/services/emotions.service.ts
+++ ng2/aplicacionDePrueba/src/app/shared/services/emotions.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers, Response } from "@angular/http";
 
-import "rxjs/add/operator/toPromise";
-
 @Injectable()
 export class EmotionsService {
 
@@ -17,13 +15,7 @@ export class EmotionsService {
       'Ocp-Apim-Subscription-Key': '49e072cd369c4e2b946357e83674fd2e'
     });
     
-    return this.hachetetepe.post(this.urlDelAPI, stringDeDatos, {headers: headers}).toPromise().then(response => {
-      return response.json();
-    })
-    .catch(err => {
-      console.log(err)
-      return err;
-    });
+    return this.hachetetepe.post(this.urlDelAPI, stringDeDatos, {headers: headers});
   }
 
 }
